1aa305474f6dc1c4e40f6bce0fd4fb4f48331f5f
FabienCOTE <fabien.cote@gmail.com>
2018-10-01 15:34:29 +0200

    Ajout du BackToTop

---
+++ assets/js/javascript.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
